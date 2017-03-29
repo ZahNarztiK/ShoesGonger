2a828fe2855334dc1c6856566466dc86ce427ff9
ZahNarztiK <ZahNarztiK@WindowsLive.com>
2017-03-30 04:24:44 +0700

    Interval > Timeout

--- popup.js
+++ popup.js
@@ -29,7 +29,7 @@ function sg_setCounter(n) { $('#sg_counter').html(n); }
 function sg_setForm()
 {
 	$('#sg_keyword').val(sg_info.owrai);
-	$('#sg_interval').val(sg_info.delayed);
+	$('#sg_timeout').val(sg_info.delayed);
 	$('#sg_clear').prop("checked",sg_info.cookiesClear);
 	$('#sg_button').attr("class",run?"stop":(sg_availKW()?"start":""));
 	$('#clear').attr("class",run?"disable":"");
@@ -79,17 +79,17 @@ $(function(){
 		chrome.storage.sync.set({cookiesClear:(sg_info.cookiesClear=$('#sg_clear').prop("checked"))});
 	});
 
-	$('#sg_interval').focus(function(){ $(this).attr("placeHolder","");	});
+	$('#sg_timeout').focus(function(){ $(this).attr("placeHolder","");	});
 
-	$('#sg_interval').focusout(function(){ $(this).attr("placeHolder","0");	});
+	$('#sg_timeout').focusout(function(){ $(this).attr("placeHolder","0");	});
 
-	$('#sg_interval').keydown(function(e){
+	$('#sg_timeout').keydown(function(e){
 		if ($.inArray(e.keyCode, [46, 8, 9, 27, 13, 110, 190]) !== -1 || (e.keyCode === 65 && (e.ctrlKey === true || e.metaKey === true)) || (e.keyCode >= 35 && e.keyCode <= 40)) return;
 		if ((e.shiftKey || (e.keyCode < 48 || e.keyCode > 57)) && (e.keyCode < 96 || e.keyCode > 105)) e.preventDefault();
 	});
 
-	$('#sg_interval').keyup(function(e){
-		chrome.storage.sync.set({delayed:(sg_info.delayed=(($('#sg_interval').val().length>0)?Number($('#sg_interval').val()):0))});
+	$('#sg_timeout').keyup(function(e){
+		chrome.storage.sync.set({delayed:(sg_info.delayed=(($('#sg_timeout').val().length>0)?Number($('#sg_timeout').val()):0))});
 		if(e.keyCode==13) sg_toggle();
 	});
 

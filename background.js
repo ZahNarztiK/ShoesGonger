3ab9dd8aa3d22a027285a706ca78f10499f3dc9a
ZahNarztiK <ZahNarztiK@WindowsLive.com>
2017-03-30 03:59:01 +0700

    Add timer
    - Add time out interval

--- background.js
+++ background.js
@@ -10,13 +10,21 @@ var	tid=0,
 	counter=0,
 	run=false;
 
-var chatRoom,chatOpen=false;
+var chatRoom,chatOpen=false,timer;
 
-function inj()
+var inj,reloadPage;
+
+function injDefault()
 {
 	console.log("- Inject "+tid+" : "+ ++counter);
 	postCmd("setCounter "+counter);
-	chrome.tabs.executeScript(tid, {code: "chkGong('"+sg_info.owrai+"');"});
+	chrome.tabs.executeScript(tid,{code:"chkGong('"+sg_info.owrai+"');"});
+}
+
+function injTime()
+{
+	clearTimeout(timer);
+	injDefault();
 }
 
 function loadSettings()
@@ -24,12 +32,45 @@ function loadSettings()
 	chrome.storage.sync.get(null,function(save){
 		console.log("-Load settings-\n"+JSON.stringify(save));
 		sg_info=sg_default;
-		for(var k in save) if(k in sg_info) sg_info[k] = save[k];
+		for(var k in save) if(k in sg_info) sg_info[k]=save[k];
 		console.log("-sg_info-\n"+JSON.stringify(sg_info));
 	});
 }
 
 function postCmd(cmd) { if(chatOpen) chatRoom.postMessage(cmd); }
+
+function reloadPageDefault()
+{
+	if(sg_info.cookiesClear)
+		chrome.browsingData.remove({
+				"since": 0
+			}, {
+				//"appcache": true,
+				//"cache": true,
+				"cookies": true,
+				//"downloads": true,
+				//"fileSystems": true,
+				//"formData": true,
+				//"history": true,
+				//"indexedDB": true,
+				//"localStorage": true,
+				//"pluginData": true,
+				//"passwords": true,
+				//"webSQL": true
+			}, function(){ chrome.tabs.reload(tid); });
+	else chrome.tabs.reload(tid);
+}
+
+function reloadPageTime()
+{
+	reloadPageDefault();
+
+	console.log("  Timer: Wait " + sg_info.delayed+" ms");
+	timer=setTimeout(function(){
+		console.log("  Timer: Timed out, Re!");
+		reloadPage();
+	},sg_info.delayed);
+}
 
 function toggle()
 {
@@ -39,22 +80,32 @@ function toggle()
 		loadSettings();
 		counter=0;
 		chrome.windows.getCurrent(function(win){
-			wid=win.id;
-			console.log(win.id);
+			console.log(" WinID: ["+(wid=win.id)+"]");
 			chrome.tabs.query({active:true,windowId:wid},function(tab){
 				console.log(" TabID: ["+(tid = tab[0].id)+"]");
-				chrome.tabs.reload(tid);
+				if(sg_info.delayed>0)
+				{
+					inj=injTime;
+					reloadPage=reloadPageTime;
+				}
+				else
+				{
+					inj=injDefault;
+					reloadPage=reloadPageDefault;
+				}
+				reloadPage();
 			});
 		});
 	}
+	else clearTimeout(timer);
 	chrome.browserAction.setIcon({path:"icon/icon16"+ (run?"r":"")+".png"});
 }
 
-chrome.extension.onConnect.addListener(function(room) {
+chrome.extension.onConnect.addListener(function(room){
 	chatRoom=room;
 	chatOpen=true;
 	console.log("*- Connected to "+room.name+" -*");
-	room.onMessage.addListener(function(msg) {
+	room.onMessage.addListener(function(msg){
 		var cmd=msg.split(" ",1)[0];
 		var tail=msg.substr(cmd.length+1);
 		console.log(cmd+"\n  ["+(tail?tail:"N/A")+"]");
@@ -73,45 +124,30 @@ chrome.extension.onConnect.addListener(function(room) {
 		}
 		console.log("* Do: "+cmd);
 	});
-	room.onDisconnect.addListener(function(msg) {
+	room.onDisconnect.addListener(function(msg){
 		chatOpen=false;
 		console.log("*- Disconnected -*");
 	});
 });
 
 chrome.runtime.onMessage.addListener(function(request,sender,sendResponse){
-	if(request.ready && run) inj();
-	else if(request.daimai!=undefined)
+	if(run)
 	{
-		console.log("  Tab ["+tid+"]: "+(request.daimai?"dai":"mai dai"));
-		if (request.daimai)
+		if(request.ready) inj();
+		else if(request.daimai!=undefined)
 		{
-			console.log("  SV: OK!!!");
-			postCmd("Stop");
-			toggle();
-		}
-		else
-		{
-			//console.log("  SV: w8 " + sg_info.delayed+" ms");
-			console.log("  SV: Re!");
-			if(sg_info.cookiesClear)
-				chrome.browsingData.remove({
-						"since": 0
-					}, {
-						//"appcache": true,
-						//"cache": true,
-						"cookies": true,
-						//"downloads": true,
-						//"fileSystems": true,
-						//"formData": true,
-						//"history": true,
-						//"indexedDB": true,
-						//"localStorage": true,
-						//"pluginData": true,
-						//"passwords": true,
-						//"webSQL": true
-					}, function(){ chrome.tabs.reload(tid); });
-			else chrome.tabs.reload(tid);
+			console.log("  Tab ["+tid+"]: "+(request.daimai?"dai":"mai dai"));
+			if (request.daimai)
+			{
+				console.log("  SV: OK!!!");
+				postCmd("Stop");
+				toggle();
+			}
+			else
+			{
+				console.log("  SV: Re!");
+				reloadPage();
+			}
 		}
 	}
 });

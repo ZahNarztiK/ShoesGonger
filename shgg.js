2565de8adc226f7f1d2fc3335509b98a7f3b0cdd
ZahNarztiK <ZahNarztiK@WindowsLive.com>
2017-03-30 18:23:53 +0700

    Remove unused code

--- shgg.js
+++ shgg.js
@@ -1,5 +1,3 @@
-//function chkGong(txt){chrome.runtime.sendMessage({daimai:($("*:contains("+txt+")").length==0)});}
-//chrome.runtime.sendMessage({ready:true});
 chrome.storage.sync.get(null,function(kw){
 	chrome.runtime.sendMessage({daimai:($("*:contains("+kw.owrai+")").length==0)});
 });

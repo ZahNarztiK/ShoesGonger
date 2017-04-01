20f2b57bef13c61113edb47771044930fc6688b1
ZahNarztiK <ZahNarztiK@WindowsLive.com>
2017-04-01 22:26:47 +0700

    Bug fix

--- shgg.js
+++ shgg.js
@@ -1,5 +1,6 @@
-(function sg_chk(){
+function sg_chk(){
 	chrome.storage.sync.get("owrai",function(kw){
 		chrome.runtime.sendMessage({daimai:($("*:contains("+kw.owrai+")").length==0)});
 	});
-})()
+}
+sg_chk();

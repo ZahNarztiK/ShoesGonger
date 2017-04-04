929b28e605437401e767ef87a048bdaebd4b68d8
ZahNarztiK <ZahNarztiK@WindowsLive.com>
2017-04-04 15:53:30 +0700

    Searching bug fix

--- shgg.js
+++ shgg.js
@@ -1,7 +1,8 @@
 $(document).click(function(){ chrome.runtime.sendMessage({yuudwoi:true}); });
 function sg_chk(){
 	chrome.storage.sync.get("owrai",function(kw){
-		chrome.runtime.sendMessage({daimai:($("*:contains("+kw.owrai+")").length==0)});
+		chrome.runtime.sendMessage({daimai:($('html').html().indexOf(kw.owrai)==-1)});
+		//chrome.runtime.sendMessage({daimai:($("*:contains("+kw.owrai+")").length==0)});
 	});
 }
 sg_chk();
